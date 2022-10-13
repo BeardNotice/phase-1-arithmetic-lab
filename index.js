7f839e95defe93621a29abf91091f18e57e043d4
Ian Flurkey <ianflurkey@gmail.com>
2022-10-13 13:23:15 -0400

    commit tree aade3e31ed7a595573132963147eb7320fff9ece

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1=31;
+const num2=2;
+let multiply=num1*num2;
+let random=Math.floor(Math.random()*100);
+const num3=9;
+const num4=5;
+let mod=num3%num4;
+let max=Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
